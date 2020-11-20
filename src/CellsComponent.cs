b163d67e41405cb100a50362d6c7be8287bf44c4
nirvik00 <nirviksaha@gatech.edu>
2020-11-20 13:50:58 -0500

    Add project files.

---
+++ src/CellsComponent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Cells
+{
+    public class CellsComponent : GH_Component
+    {
+        public CellsComponent()
+          : base("About", "about",
+              "About",
+              "Cells", "cells")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("about", "about", "about", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string s = "Created at CASE, RPI";
+            DA.SetData(0, s);
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("357b9f03-3bca-4124-a3f4-4eedbf569819"); }
+        }
+    }
+}
---
+++ src/CellsInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace Cells
+{
+    public class CellsInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "Cells";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("385eb2f5-25df-42bd-8ccb-bd24ace188c1");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                return "nirvik saha, dennis shelden";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                return "sahan@rpi.edu, sheldd@rpi.edu";
+            }
+        }
+    }
+}
